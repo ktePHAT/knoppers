20d67b3be6635aed404341b1b999fb7457fe15c3
Kevin Terschluse <kte@phatconsulting.de>
2020-01-09 00:13:11 +0100

    satus update

--- src/foo.py
+++ src/foo.py
@@ -1,4 +1,4 @@
-# modell buying list item
+# model buying list item
 
 class GroceryItem:
     
@@ -10,24 +10,34 @@ class GroceryItem:
         #pfand as switch field?
 
     def itemDescription(self):
-        return '{} : {}'.format(self.name, (self.price / 100))
-        #return self.name
+        return '{} : {}'.format(self.name, self.price) #(self.price / 100)) does not work bc of from_string, maybe harcast price into int?
 
     def apply_pledge(self):
         self.price +=  self.pledge_cost
 
+    @classmethod
+    def from_string(cls, item_str):
+        name, price = item_str.split('-')
+        return cls(name, price)
+
+
+    @classmethod
+    def set_pledge_cost(cls, amount):
+        cls.set_pledge_cost = amount
+
 # item init
 item_1 = GroceryItem('noodles' , 79)
 item_2 = GroceryItem('coke', 75)
-item_3 = GroceryItem('Cherrystrauchtomaten', 249)
-item_4 = GroceryItem('towels', 299)
+item_3 = GroceryItem.from_string('Cherrystrauchtomaten-249')
+item_4 = GroceryItem.from_string('towels-299')
 
 # collection
 items = [item_1, item_2, item_3, item_4]
 
 #testing
 item_2.apply_pledge()
-print(GroceryItem.__dict__)
+
+#print(GroceryItem.__dict__)
 
 #iteration
 for item in items:

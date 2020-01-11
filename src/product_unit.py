69a78662edcd6b26920b4cbc4fbd9f8d75d6e638
Kevin Terschluse <kte@phatconsulting.de>
2020-01-11 02:14:01 +0100

    ProductUnit class

--- src/product_unit.py
+++ src/product_unit.py
@@ -3,10 +3,21 @@ from money.currency import Currency
 
 
 class ProductUnit:
-    pass
+    
+    def __init__(self, name, price):
+        self.name  = name
+        self.price = Money(price, Currency.EUR)
+    
+    
+    def apply_pledge(self):
+        """Pfand"""
+        pledge = Money('0.25', Currency.EUR)
+        self.price += pledge
 
+    def unit_description(self):
+        return '{} : {}'.format(self.name, self.price.format('de_DE'))
 
-m = Money('9.95', Currency.EUR).format('de_DE')
-print(m)
-test = "test" 
-#fooBar
+#coke = ProductUnit('CoCa-Cola', '0.75')
+#print(coke.name)
+#print(coke.price)
+#print(coke.unit_description())
